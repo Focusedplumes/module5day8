2de4bc41842d4d0ac27d29592b2d204570582236
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-07-13 12:36:38 -0500

    route api done moving to page route

--- starter-template/src/pages/api/todo.js
+++ starter-template/src/pages/api/todo.js
@@ -2,7 +2,9 @@ import { prisma } from "@/server/prisma";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
-    res.status(200).json({ message: "Hello World!" });
+    const toDo = await prisma.toDoList.findMany({
+    });
+    res.status(200).json({ todo: toDoList });
   } else {
     res.status(404).json({ message: "We only support GET requests" });
   }
