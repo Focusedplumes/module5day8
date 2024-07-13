45be08523c3b4aee5cb6e15f679828d1a00f3cda
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-07-13 12:10:36 -0500

    schema and database displaying

--- starter-template/prisma/seed.js
+++ starter-template/prisma/seed.js
@@ -2,8 +2,53 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
+const listData =[
+  {
+   id: 1,
+   name: "House",
+   description: "Clean house.",
+   notDone: false,
+   done: true,
+  },
+  {
+    id: 2,
+    name: "Car",
+    description: "Wash car.",
+    notDone: false,
+    done: true,
+   },
+   {
+    id: 3,
+    name: "Laundry",
+    description: "Do laundry.",
+    notDone: false,
+    done: true,
+   },
+   {
+    id: 4,
+    name: "Clothes",
+    description: "Fold clothes.",
+    notDone: false,
+    done: true,
+   },
+   {
+    id: 5,
+    name: "Fun",
+    description: "Play video games.",
+    notDone: false,
+    done: true,
+   },
+];
+
 async function main() {
-  console.log(`Update me to seed data`);
+  console.log(`Feed the seed data...`);
+  for (const d of listData) {
+    const toDo = await prisma.toDoList.create({
+      data: d,
+    });
+    console.log(`Created to-do list items with id: ${toDo.id}`);
+  }
+  console.log(`Seeding done.`);
 }
 
 main()
